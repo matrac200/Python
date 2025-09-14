ed17d676127dffc660e2ee5f41a472c975b8f4bf
Андрей Боев <a.boev.it@gmail.com>
2025-09-14 17:08:51 +0300

    Разбор темы "Введение"

---
+++ 1. Введение/main.py
@@ -0,0 +1,10 @@
+"""
+print('Text 1', end=' ')
+print('Text 2')
+"""
+"""
+print('Text 1', 'Text 2', 'Text 3', sep='\n')
+"""
+"""
+print('\'Hello\'')
+"""
---
+++ 1. Введение/main3.py
@@ -0,0 +1,3 @@
+x1 = int(input())
+x2 = int(input())
+print(f'{x1} + {x2} = {x1 + x2}')
